eab39e3737e0100f96716a10f05fa604100f4f58
free-xueq-jianb <91334149+free-xueq-jianb@users.noreply.github.com>
2022-01-28 00:10:04 +0800

    Add files via upload

--- source/date_count.js
+++ source/date_count.js
@@ -12,3 +12,36 @@ if (t < 0) {
         document.getElementById("demo").innerHTML = "TIME UP";
         document.getElementById("day").innerHTML ='0';}
 }, 1000);
+
+var styles = `
+#clockdiv{
+    font-family: sans-serif;
+    color: #fff;
+    display: inline-block;
+    font-weight: 100;
+    text-align: center;
+    font-size: 30px;
+    padding: 10px;
+    border-radius: 3px;
+    background: #00BF96;
+    display: inline-block;
+}
+#clockdiv > div{
+    display: inline-block;
+}
+#clockdiv div > span{
+    padding: 2px;
+    border-radius: 3px;
+    background: #fff;
+    color: #00BF96;
+    display: inline-block;
+}
+smalltext{
+    padding-top: 5px;
+    font-size: 16px;
+}
+`
+var styleSheet = document.createElement("style")
+styleSheet.type = "text/css"
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
